0aa487bff3bb894526f80b398564c499692418d7
BursaliAlperen <bursali.alperen.131@gmail.com>
2025-06-05 23:25:45 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,29 +1,11 @@
-from flask import Flask, send_file, request, jsonify
-import time
+
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-clicks = 0
-btc_per_click = 0.0000001
-wallet = "BURAYA_BTC_ADRESİNİ_YAZ"
-
-@app.route("/", methods=["GET", "HEAD"])
-def index():
-    return send_file("index.html")
-
-@app.route("/click", methods=["POST"])
-def click():
-    global clicks
-    clicks += 1
-    return jsonify({"clicks": clicks, "reward": btc_per_click})
-
-@app.route("/stats", methods=["GET"])
-def stats():
-    return jsonify({
-        "total_clicks": clicks,
-        "btc_per_click": btc_per_click,
-        "total_reward_btc": clicks * btc_per_click
-    })
+@app.route("/")
+def home():
+    return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=10000)

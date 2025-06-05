806ff6febf6d6a7bf0cf035680ddbe98989ef3d1
BursaliAlperen <bursali.alperen.131@gmail.com>
2025-06-05 22:34:56 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
+from flask import Flask
 
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "🎉 Ads2BTC sitesi çalışıyor!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)

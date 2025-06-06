e7b862e752ae898b347b3107df54928bfd5c9195
BursaliAlperen <bursali.alperen.131@gmail.com>
2025-06-06 08:18:39 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 
-from flask import Flask, send_file
+from flask import Flask, render_template, request
+import json, os
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return send_file("index.html")
+DATA_FILE = "admin_panel/data.json"
+
+def load_data():
+    if os.path.exists(DATA_FILE):
+        with open(DATA_FILE, "r") as f:
+            return json.load(f)
+    return {}
+
+@app.route("/admin")
+def admin():
+    data = load_data()
+    return render_template("admin.html", users=data)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    app.run(debug=True)
